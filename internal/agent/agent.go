d898de8c2428db3b440b5e17d7944627ab6ad142
makinje <malcolm.akinje@gmail.com>
2025-12-06 16:05:59 -0700

    init of agent start.

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -1,7 +1,9 @@
 package agent
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
 
 	agentv1 "github.com/aero-arc/aero-arc-protos/gen/go/aeroarc/agent/v1"
 	"github.com/bluenviron/gomavlib/v3"
@@ -10,8 +12,9 @@ import (
 )
 
 type Agent struct {
-	Node   *gomavlib.Node
-	Client agentv1.AgentGatewayClient
+	node    *gomavlib.Node
+	gateway agentv1.AgentGatewayClient
+	options *AgentOptions
 }
 
 func NewAgent(options *AgentOptions) (*Agent, error) {
@@ -21,8 +24,8 @@ func NewAgent(options *AgentOptions) (*Agent, error) {
 	}
 
 	return &Agent{
-		Client: agentv1.NewAgentGatewayClient(conn),
-		Node: &gomavlib.Node{
+		gateway: agentv1.NewAgentGatewayClient(conn),
+		node: &gomavlib.Node{
 			Endpoints: []gomavlib.EndpointConf{
 				gomavlib.EndpointSerial{
 					Device: options.SerialPath,
@@ -31,9 +34,55 @@ func NewAgent(options *AgentOptions) (*Agent, error) {
 			},
 			Dialect: common.Dialect,
 		},
+		options: options,
 	}, nil
 }
 
-func (a *Agent) Start() error {
-	return fmt.Errorf("not implemented")
+func (a *Agent) Start(ctx context.Context) error {
+	if err := a.node.Initialize(); err != nil {
+		return fmt.Errorf("failed to initialize node: %v", err)
+	}
+
+	for evt := range a.node.Events() {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			if frameEvt, ok := evt.(*gomavlib.EventFrame); ok {
+				// TODO: Handle frame
+				slog.LogAttrs(
+					ctx, slog.LevelInfo,
+					"Received frame",
+					slog.String("frame-message", fmt.Sprintf("%+v", frameEvt.Message())),
+				)
+				continue
+			}
+
+			if _, ok := evt.(*gomavlib.EventChannelOpen); ok {
+				// TODO: Handle channel open
+				slog.LogAttrs(
+					ctx, slog.LevelInfo,
+					"Channel opened: for relay",
+					slog.String("relay-address", a.options.ServerAddress),
+					slog.Int("relay-port", a.options.ServerPort),
+				)
+				continue
+			}
+
+			if _, ok := evt.(*gomavlib.EventChannelClose); ok {
+				// TODO: Handle channel close
+				slog.LogAttrs(
+					ctx, slog.LevelInfo,
+					"Channel closed: for relay",
+					slog.String("relay-address", a.options.ServerAddress),
+					slog.Int("relay-port", a.options.ServerPort),
+				)
+				continue
+			}
+
+			slog.LogAttrs(ctx, slog.LevelError, "unsupported event type", slog.String("event-type", fmt.Sprintf("%T", evt)))
+		}
+	}
+
+	return nil
 }
